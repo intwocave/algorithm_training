8923cc68516ef1379acf75003991eed8a589bbf6
intwocave <intwocave@gmail.com>
2024-04-18 00:18:34 +0900

    1193.cpp

--- BOJ/1193.cpp
+++ BOJ/1193.cpp
@@ -5,13 +5,18 @@ int main(void) {
     int x,i,sum;
     cin >> x;
 
-    for(i=1, sum=1; !(x >= sum && x < sum+i); i++, sum+=i) {
-        cout << "i: " << i << ", sum: " << sum << endl;
+    for(i=1, sum=1; !(x >= sum && x < sum+i); sum+=i, i++) {
+        // cout << "i: " << i << ", sum: " << sum << endl;
     }
 
-    int idx = sum-x;
+    int idx = x-sum;
+    // cout << "idx: " << idx << endl;
 
-    cout << idx+1 << "/" << i-idx << endl;
+    if(i%2) {
+        cout << i-idx << "/" << idx+1 << endl;
+    } else {
+        cout << idx+1 << "/" << i-idx << endl;
+    }
 
     return 0;
 }
