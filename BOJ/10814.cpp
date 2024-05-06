190ad5706e5115a242aac02253eb4be9e23a01f2
intwocave <intwocave@gmail.com>
2024-05-06 13:38:25 +0900

    Osegu

--- BOJ/10814.cpp
+++ BOJ/10814.cpp
@@ -3,7 +3,9 @@
 #include <algorithm>
 using namespace std;
 
- bool cmp()
+ bool cmp(const pair<int,string> &tar, const pair<int,string> &ori) {
+    return tar.first < ori.first;
+ }
 
 int main(void) {
     vector<pair<int, string>> v;
@@ -16,8 +18,24 @@ int main(void) {
         v.push_back(make_pair(num, str));
     }
 
-    sort(v.begin(), v.end());
+    /* cout << "Before: " << endl;
+    for(auto a : v) cout << a.first << " " << a.second << '\n'; */
 
+    stable_sort(v.begin(), v.end(), cmp);
+    /* bool sw;
+    do {
+        sw = false;
+        for(int i=0; i<n-1; i++) {
+            if(v[i].first > v[i+1].first) {
+                auto a = v[i];
+                v[i] = v[i+1];
+                v[i+1] = a;
+                sw = true;
+            }
+        }
+    } while(sw); */
+
+    /* cout << "After: " << endl; */
     for(auto a : v) cout << a.first << " " << a.second << '\n';
 
     return 0;
