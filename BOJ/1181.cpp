4b814cdd75533cddf6142fd50e6bbdd09fd28b50
intwocave <intwocave@gmail.com>
2024-05-07 12:49:02 +0900

    Silver 2

--- BOJ/1181.cpp
+++ BOJ/1181.cpp
@@ -4,8 +4,9 @@
 using namespace std;
 
 bool cmp(string &v1, string &v2) {
-    if(v1.length() < v2.length()) return v1 < v2;
-    else return v1 > v2;
+    if (v1.length() == v2.length()) return v1 < v2;
+    else if (v1.length() > v2.length()) return false;
+    else return true;
 }
 
 int main(void) {
@@ -19,16 +20,19 @@ int main(void) {
         v.push_back(str);
     }
 
-    v.erase(unique(v.begin(), v.end()), v.end());
     sort(v.begin(), v.end());
+    // unique(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
 
-    cout << "--- 정렬 결과 ---" << endl;
-    int cnt = 0;
+    sort(v.begin(), v.end(), cmp);
+
+    // cout << "--- 정렬 결과 ---" << endl;
+    // int cnt = 0;
     for (auto a : v) {
         cout << a << '\n';
-        cnt++;
+        // cnt++;
     }
-    cout << "총 " << cnt << "개" << endl;
+    // cout << "총 " << cnt << "개" << endl;
 
     return 0;
 }
