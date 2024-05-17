c26617cbdb21f1578a8fc6a5cfb97475cacd9f36
intwocave <intwocave@gmail.com>
2024-05-17 23:54:51 +0900

    dead-line

--- BOJ/15650.cpp
+++ BOJ/15650.cpp
@@ -5,10 +5,22 @@ int main(void) {
     int n, m;
     cin >> n >> m;
     int arr[n+1] = {};
-    for(int i=1; i<=n; i++) {
-        arr[i] = i;
-    }
+    bool vs[n+1] = {};
+    for(int i=1; i<=n; i++) arr[i] = i;
 
+    for(int j=1; j<=m; j++) {
+        for(int i=j, cnt; i<=n; i++) {
+            cnt=0;
+            if(vs[i]==false && cnt<m) {
+                cout << arr[i] << " ";
+                if(cnt == m-1) vs[i]=true;
+                cnt++;
+            }
+        }
+    }
+    
+    for(int i=0; i<n+1; i++) vs[i]=false;
+    vs[j]=true;
     
 
     return 0;
---
+++ BOJ/27866.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int i;
+    string str;
+    cin >> str >> i;
+    cout << str[i-1];
+
+    return 0;
+}
