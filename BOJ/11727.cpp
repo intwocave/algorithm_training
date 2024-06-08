8b24672adaaf79f1e81cd65900ac42c4d969a458
intwocave <intwocave@gmail.com>
2024-06-08 23:56:02 +0900

    SOMERSBY & X1C9

--- BOJ/11727.cpp
+++ BOJ/11727.cpp
@@ -1,11 +1,9 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 int main(void) {
-	int n; cin >> n;
-	int dp[1001] = {0, 1, 3,};
-	for(int i=3; i<=1000; i++) dp[i] = dp[i-1] + dp[i-2] * 2;
-	cout << dp[n] % 10007 << endl;
-
+	int dp[1001]={0,1,3,};
+	for(int i=3;i<=1000;i++)dp[i]=(dp[i-1]+dp[i-2]*2)%10007;
+	int n;cin>>n;
+	cout<<dp[n]<<endl;
 	return 0;
 }
