9746943375196a08efba02639b056402ee9d2ef7
intwocave <intwocave@gmail.com>
2024-06-30 01:12:58 +0900

    Bowling

--- BOJ/15650.cpp
+++ BOJ/15650.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 using namespace std;
 
+int n, m;
+int arr[10], vis[10];
+
+void dfs(int num, int cnt) {
+	if(cnt==m) {
+		for(int i=0; i<m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << endl;
+		return;
+	}
+	for(int i=num; i<=n; i++) {
+		if(!vis[i]) {
+			vis[i] = 1;
+			arr[cnt] = i;
+			dfs(i+1, cnt+1);
+			vis[i] = 0;
+		}
+	}
+}
+
 int main(void) {
-    int n, m;
-    cin >> n >> m;
-    int arr[n+1] = {};
-    bool vs[n+1] = {};
-    for(int i=1; i<=n; i++) arr[i] = i;
+	cin >> n >> m;
 
-    for(int j=1; j<=m; j++) {
-        for(int i=1, cnt;;) {
-            cnt=0;
-            if(vs[i]==false && cnt<m) {
-                cout << arr[i] << " ";
-                if(cnt == m-1) vs[i]=true;
-                cnt++;
-            }
-        }
-    }
-    
-    for(int i=0; i<n+1; i++) vs[i]=false;
-    vs[j]=true;
-    
+	dfs(1, 0);
 
-    return 0;
+	return 0;
 }
