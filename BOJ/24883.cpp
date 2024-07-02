ca0912911ed54c86d8d1af9a3f8fc2782b67957b
intwocave <intwocave@gmail.com>
2024-07-03 03:25:03 +0900

    Arch btw

---
+++ BOJ/24883.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	char c; cin >> c;
+
+	if(c=='N'||c=='n') cout << "Naver D2" << endl;
+	else cout << "Naver Whale" << endl;
+
+	return 0;
+}
