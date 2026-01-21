20b99f2c393262215fb3fc173e4b81bc9380fa57
intwocave <intwocave@gmail.com>
2026-01-21 16:24:13 +0900

    add template for LC

---
+++ LeetCode/template.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  int problemName(vector<int>& nums) {
+    // Logic
+    
+    // return result;
+  }
+};
+
+int main(void) {
+  // vector<int> input_nums = {4, 1, 2, 1, 2};
+
+  Solution sol;
+
+  // int result = sol.problemName(input_nums);
+
+  // cout << result << endl;
+
+  return 0;
+}
